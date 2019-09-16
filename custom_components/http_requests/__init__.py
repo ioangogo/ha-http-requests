0645a7ebe1896937113f5835180aaf7d6f7767ab
Ioan Loosley <legit.ioangogo@gmail.com>
2019-09-16 09:30:12 +0100

    changed to the correct request and finished it

--- custom_components/http_requests/__init__.py
+++ custom_components/http_requests/__init__.py
@@ -47,8 +47,47 @@ def setup(hass, config):
 
         return resp.status_code == 200
 
+    def http_post(call):
+        """Performs a get request"""
+
+        verify_ssl = (
+            True if "verify_ssl" not in call.data.keys() else call.data["verify_ssl"]
+        )
+
+        headers = basic_headers
+        if "headers" in call.data.keys():
+            headers.update(call.data["headers"])
+
+        auth = None
+        if "auth_username" in call.data.keys() and "auth_password" in call.data.keys():
+            auth = (
+                call.data["auth_username"]
+                if "auth_username" in call.data.keys()
+                else None,
+                call.data["auth_password"]
+                if "auth_password" in call.data.keys()
+                else None,
+            )
+
+        data = None
+
+        if "data" in call.data.keys():
+            data = call.data["data"]
+
+        resp = requests.post(
+            url=call.data["url"],
+            data=data,
+            headers=headers,
+            verify=verify_ssl,
+            timeout=10,
+            auth=auth,
+        )
+
+        return resp.status_code == 200
+
     # Register our service with Home Assistant.
     hass.services.register(DOMAIN, "http_get", http_get)
+    hass.services.register(DOMAIN, "http_post", http_post)
 
     # Return boolean to indicate that initialization was successfully.
     return True
